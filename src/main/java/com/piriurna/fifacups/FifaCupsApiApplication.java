4e8a50514f84cb92a3057fe7aa1b0814f169be68
FelipeRavaCardona <feliperavacardona@hotmail.com>
2024-05-21 13:31:26 -0300

    base project

---
+++ src/main/java/com/piriurna/fifacups/FifaCupsApiApplication.java
@@ -0,0 +1,13 @@
+package com.piriurna.fifacups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaCupsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaCupsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piriurna/fifacups/FifaCupsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piriurna.fifacups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaCupsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
