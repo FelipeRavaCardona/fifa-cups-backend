e9b5ca120bf484894c973202cc1bdba8f45cb6f4
FelipeRavaCardona <feliperavacardona@hotmail.com>
2024-05-25 19:59:16 -0300

    updating infra

--- infrastructure/api_gateway/main.tf
+++ infrastructure/api_gateway/main.tf
@@ -3,10 +3,10 @@ resource "aws_apigatewayv2_api" "this" {
     protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "this" {
+resource "aws_apigatewayv2_integration" "proxy" {
     api_id                 = aws_apigatewayv2_api.this.id
     integration_type       = "HTTP_PROXY"
-    integration_uri        = "http://${var.public_dns}/{proxy}"
+    integration_uri        = "http://${var.public_dns}:8080/{proxy}"
     integration_method     = "ANY"
     payload_format_version = "1.0"
 }
@@ -14,12 +14,18 @@ resource "aws_apigatewayv2_integration" "this" {
 resource "aws_apigatewayv2_route" "proxy" {
     api_id    = aws_apigatewayv2_api.this.id
     route_key = "ANY /{proxy+}"
-    target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+    target    = "integrations/${aws_apigatewayv2_integration.proxy.id}"
+}
+
+resource "aws_apigatewayv2_integration" "default" {
+    api_id = aws_apigatewayv2_api.this.id
+    integration_type = "HTTP_PROXY"
 }
 
 resource "aws_apigatewayv2_route" "default" {
     api_id    = aws_apigatewayv2_api.this.id
     route_key = "$default"
+    target    = "integrations/${aws_apigatewayv2_integration.default.id}"
 }
 
 resource "aws_apigatewayv2_stage" "this" {
